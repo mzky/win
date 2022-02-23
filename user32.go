f5c4470a58db6f87c348d2e15fe9a13b8ebdbe4c
Mengdch <mengdc@gmail.com>
2022-02-23 19:06:35 +0800

    添加常量

--- user32.go
+++ user32.go
@@ -64,6 +64,13 @@ const (
 	IDCONTINUE = 11
 	IDTIMEOUT  = 32000
 )
+const (
+	SIZE_RESTORED  = 0
+	SIZE_MINIMIZED = 1
+	SIZE_MAXIMIZED = 2
+	SIZE_MAXSHOW   = 3
+	SIZE_MAXHIDE   = 4
+)
 
 // System commands
 const (
